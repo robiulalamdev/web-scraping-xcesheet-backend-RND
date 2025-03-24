79b26f6a1b1c5dcf1f2c82a5b6e152509386f713
robiulalamdev <robiulalam762400@gmail.com>
2025-03-25 01:31:12 +0600

    refactor: simplify category retrieval logic and clean up unused code

--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
 const express = require("express");
-const multer = require("multer");
-const xlsx = require("xlsx");
-const fs = require("fs");
-const path = require("path");
 const cors = require("cors");
 const { getDescription } = require("./scrape");
 const { SSE } = require("./sse/sseServer");
-const { connect } = require("http2");
 
 const app = express();
 app.use(cors());
@@ -14,69 +9,6 @@ app.use(express.json());
 
 app.get("/sse-connect/:id", SSE.initialize);
 
-// if not exist uploads/ folder, create it
-if (!fs.existsSync("uploads")) {
-  fs.mkdirSync("uploads");
-}
-const upload = multer({ dest: "uploads/" });
-
-// Upload & Process Excel File
-// app.post("/upload-excel", upload.single("file"), async (req, res) => {
-//   if (!req.file) {
-//     return res.status(400).json({ error: "No file uploaded" });
-//   }
-
-//   try {
-//     const filePath = req.file.path;
-//     const workbook = xlsx.readFile(filePath);
-//     const sheetName = workbook.SheetNames[0];
-//     let data = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
-
-//     // Ensure "description" exists in each object
-
-//     let newData = [];
-//     for (let i = 0; i < data.length; i++) {
-//       const row = data[i];
-//       console.log("No: ", i, "  ", row?.Part);
-//       if (row?.Part) {
-//         const result = await getDescription(row?.Part);
-//         newData.push({
-//           ...row,
-//           description: result?.description || "Not found",
-//         });
-//       } else {
-//         newData.push({
-//           ...row,
-//           description: "Not found",
-//         });
-//       }
-//     }
-//     // const result = await getDescription("M87686-601");
-//     // newData.push({
-//     //   ...data[0],
-//     //   description: result?.description || "Not found",
-//     // });
-
-//     data = newData;
-
-//     // Convert updated data back to Excel
-//     const newWorkbook = xlsx.utils.book_new();
-//     const newWorksheet = xlsx.utils.json_to_sheet(data);
-//     xlsx.utils.book_append_sheet(newWorkbook, newWorksheet, "UpdatedData");
-
-//     // Save the updated file
-//     const newFilePath = `uploads/updated_${req.file.originalname}`;
-//     xlsx.writeFile(newWorkbook, newFilePath);
-
-//     // Delete the original file
-//     fs.unlinkSync(filePath);
-
-//     res.json({ filePath: newFilePath, data });
-//   } catch (error) {
-//     console.error("Error processing file:", error);
-//     res.status(500).json({ error: "Internal server error" });
-//   }
-// });
 app.post("/scrape", async (req, res) => {
   const { sheets = [], connectionId } = req.body;
   if (!sheets || sheets?.length === 0) {
@@ -127,26 +59,8 @@ app.post("/scrape", async (req, res) => {
   }
 });
 
-// Serve files for download
-app.get("/download/:fileName", (req, res) => {
-  const filePath = path.join(__dirname, "../uploads", req.params.fileName);
-  console.log(filePath);
-  if (fs.existsSync(filePath)) {
-    res.download(filePath);
-  } else {
-    res.status(404).json({ error: "File not found" });
-  }
-});
-
-// Delete file
-app.delete("/delete/:fileName", (req, res) => {
-  const filePath = path.join(__dirname, "uploads", req.params.fileName);
-  if (fs.existsSync(filePath)) {
-    fs.unlinkSync(filePath);
-    res.json({ message: "File deleted successfully" });
-  } else {
-    res.status(404).json({ error: "File not found" });
-  }
+app.get("/", (req, res) => {
+  res.send("API version 1.0.0");
 });
 
 app.listen(9700, () => console.log("Server running on port 3000"));
